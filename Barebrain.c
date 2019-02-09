2a3ddd6e592bdaf2ee563656593b0e9eb531503a
Patrick Bowen <phun@protonmail.com>
2019-02-09 02:30:27 +0000

    Spring clean; minor heuristic improvement

--- Barebrain.c
+++ Barebrain.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include <stdint.h>
 
-#define TAPE_LEN	10000
-#define PROG_SIZE	10000
+#define PROG_SIZE	4096
+#define TAPE_LEN	1024
 #define LOOP_MAX	256
 
 enum Symbols { INC, DEC, RIG, LEF, LOO, END, PUT, GET, ZER, EOP };
 
-int main (int argc, char *argv[])
+int main (int argc, char* argv[])
 {
 	//Check if program was supplied, and attempt to open
-	FILE *file;
+	FILE* file;
 	if (argc != 2 || (file = fopen(argv[1], "r")) == NULL) {
-		puts("Err: Supply program filename as sole argument.");
+		puts("Supply program filename as sole argument.");
 		return 1;
 	}
 
@@ -26,8 +26,7 @@ int main (int argc, char *argv[])
 	uint16_t o = 0; //Opcode iterator
 	//Extract op codes and their repeat counts, with some optimisation
 	{
-		uint16_t a = 0; //Input iterator
-		int8_t ch; //Character buffer
+		int8_t ch; //Char buffer
 		while ((ch = getc(file)) != EOF) {
 
 			//Filter for valid op's
@@ -39,8 +38,9 @@ int main (int argc, char *argv[])
 				default: continue;
 			}
 
-			//Tally repeated op (ex. loop)
-			if (ch == arr_p[o] && ch != LOO && ch != END) ++arr_r[o];
+			//Tally repeated op (ex. loop), else prepare for unique op
+			if (ch == arr_p[o] && ch != LOO && ch != END)
+				++arr_r[o];
 			else {
 				++o;
 				if (o == PROG_SIZE - 1) {
@@ -52,47 +52,46 @@ int main (int argc, char *argv[])
 			}
 
 			//Check for [-]
-			if (arr_p[o] == END && o > 2 && arr_p[o-1] == DEC && arr_p[o-2] == LOO) {
-				o -= 2;
-				arr_p[o] = ZER;
-			}
+			if (arr_p[o] == END && o > 2 && arr_p[o-1] == DEC && arr_p[o-2] == LOO)
+				arr_p[o -= 2] = ZER;
 		}
 	}
 	fclose(file);
 	
-	uint8_t *p = arr_p;	//Program pointer
-	*(p+o+1) = EOP;		//Append End-Of-Program
+	arr_p[o+1] = EOP;	//Append End-Of-Program
+	uint8_t* p = arr_p;	//Program pointer
 
 	//Generate loop offset heuristics
 	{
-		uint8_t* inception[LOOP_MAX];
-		uint8_t** i = inception;
+		uint8_t* loops[LOOP_MAX];
+		uint8_t** l = loops - 1;
 
 		while (*(++p) != EOP) {
-			if (*p == LOO) *(++i) = p; //Append to inception queue
+			if (*p == LOO)
+				*(++l) = p; //Append to loop queue
 			else if (*p == END) {
-				uint16_t offset = p - *i;
-				arr_el[p - arr_p] = offset;		//
-				arr_le[*i - arr_p] = offset;	// Store offsets
-				--i;
+				uint16_t offset = p - *l;
+				arr_el[p  - arr_p] = offset;	//
+				arr_le[*l - arr_p] = offset;	// Store offsets
+				--l;
 			}
 		}
 		p = arr_p; //Reset
 	}
 
 	//Prepare pointers and reusables
-	uint8_t *t = arr_t;		//Tape pointer
-	uint8_t *r = arr_r;		//Repeat pointer
-	uint16_t offset;		//Stores loop offset
+	uint8_t* t = arr_t;		//Tape pointer
+	uint8_t* r = arr_r;		//Repeat pointer
+	uint16_t offset;		//Loop offset cache
 
-	//Begin evaluating program
+	//Evaluate program
 	uint8_t do_run = 1;
 	while (do_run) {
 		switch (*p) {
-			case INC: *t += *r;	break;
-			case DEC: *t -= *r;	break;
-			case RIG: t += *r;	break;
-			case LEF: t -= *r;	break;
+			case INC: *t += *r; break;
+			case DEC: *t -= *r; break;
+			case RIG:  t += *r; break;
+			case LEF:  t -= *r; break;
 			case LOO:
 				if (*t) break;
 				offset = arr_le[p - arr_p];
@@ -113,6 +112,4 @@ int main (int argc, char *argv[])
 		++p;
 		++r;
 	}
-
-	return 0;
 }
